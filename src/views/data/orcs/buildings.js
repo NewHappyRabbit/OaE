46a5eb3c234251b846f94a779b168065e4f25b8f
NewHappyRabbit <31893343+NewHappyRabbit@users.noreply.github.com>
2023-09-26 23:56:57 +0300

    added images

--- src/views/data/orcs/buildings.js
+++ src/views/data/orcs/buildings.js
@@ -1,3 +1,16 @@
 export const orcsBuildings = [
-    //TODO
+    {
+        img: "/images/orcs/buildings/altar.webp",
+        imgR: "/images/orcs/buildings/altarR.webp",
+        name: "Altar",
+        description: "*The Altar* is used to research upgrades for your unit. Located in the top-left corner of the map.",
+        // TODO upgrades: [ conjuyrer, dmg, ...]
+    },
+    {
+        img: "/images/orcs/buildings/morph.webp",
+        imgR: "/images/orcs/buildings/morphR.webp",
+        name: "Morph",
+        description: "This building is used to morph your unit into a stronger unit with more abilities. Located in the top-left corner of the map.",
+        //TODO upgrades [morphs, and then for each morph, upgrades]
+    }
 ];
--- src/views/data/orcs/spellbook.js
+++ src/views/data/orcs/spellbook.js
@@ -6,7 +6,7 @@ export const spellBookAbilities = [
         hotkey: "W",
         mana: "25/20/15",
         cooldown: "60/30/0",
-        description: "<p>^Level 1^: Gain vision over ^600^ radius for ^6^ seconds and reveal invisible units. %%(Requires &/orcs/buildings/treeOfTechnology#Magic& *5*)%%</p><p>^Level 2^: </p>" //TODO
+        description: "<p>^Level 1^: Gain vision over ^600^ radius for ^6^ seconds and reveal invisible units. %%(Requires &/orcs/buildings/altar#Conjurer& *1*)%%</p><p>^Level 2^: Gain vision over ^600^ radius for ^8^ seconds and reveal invisible units. The effect persists on workers for up to ^3^ seconds after leaving the area. %%(Requires &/orcs/buildings/altar#Conjurer& *3*)%%</p><p>^Level 3^: Gain vision over ^600^ radius for ^10^ seconds and reveal invisible units. The effect persists on workers for up to ^5^ seconds after leaving the area. All &/elves/units/ElvenWorker#Watching Owl&*s* in the region are dispelled. %%(Requires &/orcs/buildings/altar#Conjurer& *5*)%%</p>"
     }
     //TODO
 ];
